e41d13fb959dec02480bc039f7585d3fd19b4dbf
mustafasheikh1 <mustafaahmed726@gmail.com>
2020-02-21 07:25:58 +0500

    changes

--- src/audio.js
+++ src/audio.js
@@ -1,79 +1,99 @@
 $(document).ready(function () {
-	$.ajax({
-		url: "./assets/ayats.json", success: function (result) {
-			initialize(result);
-		}
-	}).done(() => {
-		$(`audio:visible[id*='audio']`).on('timeupdate', function (event) {
-			audio = event.currentTarget;
-			spns_id = audio.getAttribute("data_target");
-			spns = $(`#${spns_id}`).children();
-			for (i = 0; i < spns.length; i++) {
-				if (parseFloat(audio.currentTime) >= parseFloat(spns[i].id.slice(2))) {
-					spns[i].style.backgroundColor = "red";
-				}
-			}
-		});
+	var audioElement = document.createElement('audio');
+	audioElement.setAttribute('src', `assets/audio.mp3`);
+	$.ajax({url: "./assets/timestamps.json"}).done((res) => {
+		var lineTimes = [0,71,147]
+		// console.log(res.timestamps)
+		var timestamps = res.timestamps
+		var index = 0;
+
+		$(`p:visible[id*='ayat']`).each(function (i, textArray){
+			var characters = textArray.innerHTML.split("")
+			var elem = document.createElement('p')
+			elem.setAttribute('id', 'ayat_')
+			elem.setAttribute('key', i+1)
+			elem.style.color = '#C0C0C0';
+
 	
-		$(`audio:visible[id*='audio']`).on('ended', function (event) {
-			audio = event.currentTarget;
+			$.each(characters, function (j, el) {
+				_span = document.createElement('span')
+				// console.log(el.charCodeAt(0))
+				if(el == " "){
+					_span.setAttribute("id", 'ts-1')
+					_span.innerHTML = el
+					elem.append(_span)
+				}else{
+					if(i < 2 && el.charCodeAt(0) <= 1610 && el.charCodeAt(0) >= 1571) {
+						_span.setAttribute("id", `${timestamps[index]}`)
+						index++;
+					}else{
+						_span.setAttribute("id", `ts1`)
+					}
+					_span.setAttribute("target", i+1)
+					_span.setAttribute("target", i+1)
+					_span.setAttribute("highlighted", false)
+					_span.innerHTML = el
+					elem.append(_span)
+				}
+			})
+			$('#ayat').replaceWith(elem)
 			
-			spns_id = audio.getAttribute("data_target");
-			spns = $(`#${spns_id}`).children();
-			for (i = 0; i < spns.length; i++) {
-				spns[i].style.backgroundColor = "white";
-			}
 		})
+	
+		$(`p:visible[id*='ayat_']`).on('click', function (event) {
+			ayat = event.currentTarget
+			key = Number(ayat.getAttribute("key"))
+			word = 1;
+			// console.log(audioElement.paused)
+			
+	
+			if(audioElement.paused){
+				audioElement.setAttribute('data_target', `${key}`);
+				// console.log(key)
+				// console.log(lineTimes[key-1])
+				audioElement.currentTime = lineTimes[key-1]
+				audioElement.play()
+		
+				body = $(`body`);
+				body.append(audioElement)
+				
+	
+				$(`audio`).on('timeupdate', function () {
+					spns_id = audioElement.getAttribute("data_target");
+					spns = $(`[target='${spns_id}'`)
+					for (i = 0; i < spns.length; i++) {
+						if (parseFloat(audioElement.currentTime) >= parseFloat(spns[i].id.slice(2))) {
+							// console.log(spns[i])
+							spns[i].style.color = "#000000";
+						}
+					}
+					if(key != 3){
+						if(audioElement.currentTime >= lineTimes[key]){
+							$('audio').remove()
+						}
+					}
+				})
+				
+				$(`audio`).on('ended', function (event) {
+					$('audio').remove()
+					console.log('ended')
+				})	
+				
+				$(`audio`).on('pause', function (event) {
+					spns_id = audioElement.getAttribute("data_target");
+					spns = $(`[target='${spns_id}'`)
+					for (i = 0; i < spns.length; i++) {
+						if (parseFloat(audioElement.currentTime) >= parseFloat(spns[i].id.slice(2))) {
+							// console.log(spns[i])
+							spns[i].style.color = "#C0C0C0";
+						}
+					}
+					$('audio').remove()
+					console.log('ended')
+				})
+			} else{
+				audioElement.pause()
+			} 
+		});
 	});
-
-
 })
-
-
-function initialize(data) {
-	data_ = data.ayats
-	data = data.ayats[0]
-	var ayats = $('#ayats');
-
-	for (i = 0; i < data_.length; i++) {
-		var sentence = document.createElement("div");
-		sentence.setAttribute('id','sentence')
-		var audio_ = document.createElement("audio");
-		var audioSource = document.createElement("source");
-
-		audio_.setAttribute('id', `audio`)
-		audio_.setAttribute('data_target', `_${data_[i].id}`)
-		audio_.setAttribute('controls', '')
-		audioSource.setAttribute('src', `${data_[i].audio_src}`)
-
-		//audio
-		audio_.append(audioSource);
-
-		//sentence
-		var word = document.createElement("pre")
-		word.setAttribute("id", `_${data_[i].id}`)
-		console.log(data_)
-
-		for (j = 0; j < data_[i].words.length; j++) {
-			for (x = 0; x < data_[i].words[j].length; x++) {
-				var alpha = document.createElement("span")
-				alpha.setAttribute(`id`, `ts${data_[i].words[j][x].ts}`)
-				alpha.innerHTML = `${data_[i].words[j][x].alpha}`
-				word.append(alpha)
-			}
-			word.append(' ')
-		}
-
-		sentence.append(word)
-
-		var card = document.createElement('div')
-		card.setAttribute('id', 'ayat')
-
-		card.append(audio_)
-		card.append(sentence)
-
-		$('#ayats').append(card)
-
-	}
-	console.log(ayats)
-}
